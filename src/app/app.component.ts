5b9aae28606a6f20cb6763b05239cebc8046338c
Natalia <Natalia.kutina13@gmail.com>
2025-05-21 10:45:48 +0200

    rename titles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,15 +3,14 @@ import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
 import {RouterModule} from '@angular/router';
-import { CrownComponent } from "./icons/crown/crown.component";
 
 
 @Component({
   selector: 'app-root',
-  imports: [MatSlideToggleModule, MatButtonModule, MatIconModule, RouterModule, CrownComponent, CrownComponent],
+  imports: [MatSlideToggleModule, MatButtonModule, MatIconModule, RouterModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'Princesschool Biebouders';
+  title = 'School library';
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,10 +7,10 @@ import { RemoveBookComponent } from './components/remove-book/remove-book.compon
 import { BooksOverviewComponent } from './components/books-overview/books-overview.component';
 
 export const routes: Routes = [
-    { path: '', component: HomepageComponent, title: 'Biebouders' },
-    { path: 'add-new-book', component: AddNewBookComponent, title: 'Biebouders | Add new book' },
-    { path: 'remove-book', component: RemoveBookComponent, title: 'Biebouders | Remove book' },
-    { path: 'borrow-book', component: BorrowBookComponent, title: 'Biebouders | Borrow book' },
-    { path: 'return-book', component: ReturnBookComponent, title: 'Biebouders | Return book' },
-    { path: 'books-overview', component: BooksOverviewComponent, title: 'Biebouders | Overview' },
+    { path: '', component: HomepageComponent, title: 'School library' },
+    { path: 'add-new-book', component: AddNewBookComponent, title: 'School library | Add new book' },
+    { path: 'remove-book', component: RemoveBookComponent, title: 'School library | Remove book' },
+    { path: 'borrow-book', component: BorrowBookComponent, title: 'School library | Borrow book' },
+    { path: 'return-book', component: ReturnBookComponent, title: 'School library | Return book' },
+    { path: 'books-overview', component: BooksOverviewComponent, title: 'School library | Overview' },
 ];
