2d290e48470e8e27a21a7ba71d493eb7c9a32eda
Natalia <Natalia.kutina13@gmail.com>
2025-08-30 20:34:16 +0200

    copy to clipboard email and password

--- src/app/components/welcome/welcome.component.ts
+++ src/app/components/welcome/welcome.component.ts
@@ -1,11 +1,25 @@
 import { Component } from '@angular/core';
+import { IconCopyComponent } from '../../icons/copy/icon-copy.component';
+import { ClipboardModule } from '@angular/cdk/clipboard';
 
 @Component({
   selector: 'app-welcome',
-  imports: [],
+  imports: [IconCopyComponent, ClipboardModule],
   templateUrl: './welcome.component.html',
   styleUrl: './welcome.component.css'
 })
 export class WelcomeComponent {
+  email = "test@example.com";
+  password = "Bieb150";
+
+  copyEmail() {
+    navigator.clipboard.writeText(this.email).then().catch(e => console.log(e));
+
+  }
+
+  copyPassword() {
+    navigator.clipboard.writeText(this.password).then().catch(e => console.log(e));
+
+  }
 
 }
